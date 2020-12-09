8c3ba3a9c8b2472179414ab31b766eca331e6ef1
AmineBendriouchDev <aminebendriouch.dev@gmail.com>
2020-12-09 21:21:43 +0100

    Test1

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var scanner = new Instascan.Scanner({
+  video: document.getElementById('preview'),
+  scanPeriod: 5,
+  mirror: true,
+});
+
+scanner.addListener('scan', function (content) {
+  window.location.href = 'valider.php?qrcode=' + content;
+  document.getElementById('preview').classList.add('remove-detail');
+});
+
+Instascan.Camera.getCameras()
+  .then(function (cameras) {
+    if (cameras.length > 0) {
+      // Change camera 0=front 1=back
+      scanner.start(cameras[0]);
+    } else {
+      console.error('No cameras found.');
+      alert('No cameras found.');
+    }
+  })
+  .catch(function (e) {
+    console.error(e);
+    alert(e);
+  });
+
+function closeDetail() {
+  document.getElementById('detail').classList.remove('show-detail');
+}
